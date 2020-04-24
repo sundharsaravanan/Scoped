94395a619d0474b2bd57e8de52a64982e4301756
Sundhar Sarvanan <sundharsaravananlkp@gmail.com>
2020-04-24 21:30:24 +0530

    Initial Commit

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="container">
+  <div class="post">
+    <h4>Posts</h4>
+  </div>
+<div class="posts">
+    <div class="blog-card">
+    <div class="img">
+      <img src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="Card image" style="width:100%">
+  </div>
+  <div class="heading">
+  <a href="">Vuex state maintance in vue js</a>
+  </div>
+  <div class="info-desc">
+    What is state maintanance ? why we use vuex ? how to use vuex.
+  </div>
+</div>
+  <div class="blog-card">
+    <div class="img">
+      <img src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="Card image" style="width:100%">
+  </div>
+  <div class="heading">
+  <a href="">Vuex state maintance in vue js</a>
+  </div>
+  <div class="info-desc">
+    What is state maintanance ? why we use vuex ? how to use vuex.
+  </div>
+</div>
+<div class="blog-card">
+    <div class="img">
+      <img src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="Card image" style="width:100%">
+  </div>
+  <div class="heading">
+  <a href="">Vuex state maintance in vue js</a>
+  </div>
+  <div class="info-desc">
+    What is state maintanance ? why we use vuex ? how to use vuex.
+  </div>
+</div>
+</div>
+</div>
+</template>
+<style scoped>
+.blog-card{
+  width: 450px;
+}
+.post{
+  text-align: left;
+  margin: 20px 10px 20px 10px;
+  font-family: 'Arvo', serif;
+}
+.heading{
+    font-family: 'Arvo', serif;
+    margin-top: 10px;
+    font-size: 25px;
+    text-align: left;
+    font-weight: bold;
+}
+.heading>a{
+  color: black;
+}
+.heading>a:hover{
+  text-decoration: none;
+  color: blue;
+}
+.info-desc{
+  font-family: 'Arvo', serif;
+  margin-top: 5px;
+  font-family: 'Arvo', serif;
+  font-size: 17px;
+  text-align: left;
+}
+.posts{
+    grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+    grid-gap: 20px;
+    display: grid;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="home">
+<!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="/">Scoped</a>
+    </li>
+  </ul>
+</nav> -->
+<header class="header">
+  <div class="container">
+    <a href="/" class="header-link">Scoped</a>
+  </div>
+</header>
+</div>
+</template>
+<style scoped>
+nav{
+    font-family: 'Arvo', serif;
+    font-size: 20px;
+}
+.header{
+    position: fixed;
+    background-color: white;
+    width: 100%;
+    height: 4rem;
+    font-family: 'Arvo', serif;
+    color: white;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+}
+.header-link{
+  text-decoration: none;
+  color: blue;
+  font-weight: bold;
+}
+.header-link:hover{
+  color: lightskyblue;
+}
+</style>
