472ce87aebb0e7bad0beb606e38e8151e2a5fdef
unknown <sundharsaravananlkp@gmail.com>
2020-04-24 22:10:44 +0530

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header/>
+    <BlogCard/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from '../components/Header.vue';
+import BlogCard from '../components/BlogCard.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Header,BlogCard
   }
 };
 </script>
